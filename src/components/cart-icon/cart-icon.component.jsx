ecd97f3d781e78faec2d359069289941c5e25181
Rodrigo Castro <rodrigocastro@iMac.lan>
2020-06-18 23:43:37 +0200

    111. Implementing Redux in Cart - Figuring out how to hidden or show our CardDropdown. Also finding a way to put this functionality outside of our header component and inside our global redux state // T.: added cartIcon, cartDropdwon components to our application and implemented cart reducer.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -11,4 +14,11 @@ const CartIcon = ({ toggleCartHidden }) => (
 </div>
 );
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+
+export default connect(
+null,
+mapDispatchToProps
+)(CartIcon);
